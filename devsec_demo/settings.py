6aab95ba6dbf9ef7b2faa4d9f0cc1f9c5b0886ab
Munyabugingo <you@example.com>
2026-04-14 11:07:28 +0200

    Initial commit

--- devsec_demo/settings.py
+++ devsec_demo/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
